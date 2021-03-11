d8a0d98b47cee689010a5f37f39797f79978a114
William Wong <williamwongcastle@gmail.com>
2021-03-11 11:56:54 -0500

    core argument tooltips

---
+++ Assets/Scripts/UI/CoreArgumentTooltip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class CoreArgumentTooltip : MonoBehaviour
+{
+
+    public AbstractArgument argumentRef;
+    public Image bg;
+    private TextMeshProUGUI argumentName;
+    private TextMeshProUGUI argumentDesc;
+
+    void OnEnable(){
+        argumentRef = new CoreDeckard();
+        bg = transform.Find("BG").gameObject.GetComponent<Image>();
+        argumentName = transform.Find("Core Argument Name").gameObject.GetComponent<TextMeshProUGUI>();
+        argumentDesc = transform.Find("Core Argument Description").gameObject.GetComponent<TextMeshProUGUI>();
+
+        if (argumentRef != null){
+            argumentName.text = argumentRef.NAME;
+            argumentDesc.text = argumentRef.DESC;
+        } else {
+            argumentName.text = "MISSING ARGUMENT REF";
+        }
+    }
+}
